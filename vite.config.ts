fc350acfa302333c645ea42de1d16b679cb84826
andrt <alifandarta26@gmail.com>
2025-06-22 14:17:12 +0700

    fix: change asset image path part 5

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  assetsInclude: ['**/*.svg', '**/*.png', '**/*.jpg', '**/*.jpeg'],
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
