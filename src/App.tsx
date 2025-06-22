c5ee9a76ccc7a096aa621eb70d54e8d05d7619e0
andrt <alifandarta26@gmail.com>
2025-06-22 07:11:03 +0700

    build: upgrade vite version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { ProfileProvider } from './contexts/ProfileContext';
 import Header from './components/Header';
